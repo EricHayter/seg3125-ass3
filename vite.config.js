71580ae03a7abd28db04e37de6ebe7bf00904a83
Eric <hayter.eric@gmail.com>
2025-06-22 10:02:02 -0400

    Fix vite config for hosting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/seg3125-ass3/',
   plugins: [react()],
 })
+
